80d7ef4d91e7bcd9de8404d62a138a787385b95a
Oleksii Shcherbak <oleksii_shcherbak@icloud.com>
2025-09-29 00:16:18 +0300

    Completed task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,33 +1,11 @@
-class StringBuilder {
-  #value;
+const inputNameField = document.querySelector('#name-input');
+const spanNameField = document.querySelector('#name-output');
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-
-  getValue() {
-    return this.#value;
-  }
-
-  padEnd(str) {
-    this.#value += str;
-  }
-
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-
-  padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
-  }
+function onInputNameFieldChange(event) {
+  spanNameField.textContent =
+    event.currentTarget.value.trim() === ''
+      ? 'Anonymous'
+      : event.currentTarget.value.trim();
 }
 
-const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
-builder.padStart('^');
-console.log(builder.getValue()); // "^."
-builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
-builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
+inputNameField.addEventListener('input', onInputNameFieldChange);
